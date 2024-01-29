c298828e918ed4f0b93fabb4d66f238c094ba878
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-01-29 15:19:26 +0100

    first commit

---
+++ database/seeders/TechnologiesSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+class TechnologiesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
