337a35aaf74c8f90cae4ad45df2fc0646edac348
riccardococciolo <73590570+riccardococciolo@users.noreply.github.com>
2024-01-29 15:31:44 +0100

    add: technologies seeder

--- database/seeders/TechnologiesSeeder.php
+++ database/seeders/TechnologiesSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
@@ -16,6 +17,13 @@ class TechnologiesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $technologies = ['laravel', 'js', 'tutorial', 'lezione', 'lifehack', 'php'];
+
+        foreach ($technologies as $technology) {
+            $newTechnology = new Technology();
+            $newTechnology->name = $technology;
+            $newTechnology->slug = Str::slug($technology);
+            $newTechnology->save();
+        };
     }
 }
